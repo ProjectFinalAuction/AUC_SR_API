4526ea8f90c49ade5e6969bb7f29c1e8026df767
Chhuon <abc.ekchhuon@gmail.com>
2016-08-13 14:37:31 +0700

    Updated Category

--- src/main/java/org/khmeracademy/auction/controllers/CategoryController.java
+++ src/main/java/org/khmeracademy/auction/controllers/CategoryController.java
@@ -2,12 +2,15 @@ package org.khmeracademy.auction.controllers;
 
 import java.util.Map;
 
+import org.khmeracademy.auction.entities.input.AddCategory;
+import org.khmeracademy.auction.entities.input.AddSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,20 @@ public class CategoryController {
 	public ResponseEntity<Map<String , Object>> findAllCategories(){
 		HttpEntity<Object> request = new HttpEntity<Object>(header);
 		ResponseEntity<Map> response = rest.exchange(WS_URL + "/find-all-categories", HttpMethod.GET , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/find-main-category", method = RequestMethod.GET)
+	public ResponseEntity<Map<String , Object>> findMainCategories(){
+		HttpEntity<Object> request = new HttpEntity<Object>(header);
+		ResponseEntity<Map> response = rest.exchange(WS_URL + "/find-main-category", HttpMethod.GET , request , Map.class) ;
+		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Map<String , Object>> addCategory(@RequestBody AddCategory addCategory){
+		HttpEntity<Object> request = new HttpEntity<Object>(addCategory,header);
+		ResponseEntity<Map> response = rest.exchange(WS_URL + "/add-category", HttpMethod.POST , request , Map.class) ;
 		return new ResponseEntity<Map<String , Object>>(response.getBody() , HttpStatus.OK);
 	}
 }
--- src/main/webapp/resources/scripts/category-angular.js
+++ src/main/webapp/resources/scripts/category-angular.js
@@ -14,7 +14,44 @@ app.controller('myCtrl', function($scope,$http,$rootScope){
 			$scope.category = respone.data.DATA;
 		});
 	}
-
+	//Select Main Category
+	$scope.findMainCategories = function(){
+		$http({
+			url: 'http://localhost:8080/rest/category/find-main-category',
+			method: 'GET',
+			
+		}).then(function(respone){
+			$scope.category = respone.data.DATA;
+		});
+	}
+	
+	//AddSupplier
+	$scope.addCategory = function(){
+		
+		$http({
+			url: 'http://localhost:8080/rest/category',
+			method: 'POST',
+			data:{
+			  			  
+				"category_description": $scope.category_description,
+				"category_name":$scope.category_name,
+				"parent_id": $scope.parent_id,
+				"status": $scope.status
+	  			  
+			}
+		}).then(function(respone){
+			swal({ 
+				title: "Success!",
+				text: "Category has been inserted.",
+			    type: "success" 
+			  },
+			  function(){
+			    window.location.href = 'http://localhost:8080/admin/viewcategory';
+			});
+		});
+	}
+	
+	
 	$scope.loadme = function(){
 		alert("me");
 	};
