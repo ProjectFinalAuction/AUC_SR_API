b16d469f2f9d5f7b63caa11e1a2923144954776a
hiemseyha <hiemseyha168@gmail.com>
2016-08-26 00:22:32 +0700

    user

--- src/main/webapp/WEB-INF/pages/admin/viewuser.jsp
+++ src/main/webapp/WEB-INF/pages/admin/viewuser.jsp
@@ -88,8 +88,8 @@
 										<td ng-if="u.status=='0'"><i class="fa fa-times fa-lg text-danger"
 												aria-hidden="true"></i></td>
 										<td>{{u.type}}</td>
-										<td><a href="#" data-toggle="modal" data-target=".enterData" 
-												class="table-action-btn" ng-click="getUserByID(u)">
+										<td><a href="" data-toggle="modal" data-target=".enterData" 
+												class="table-action-btn" ng-click="getUserByID(u.user_id)">
 												<i class="md md-edit text-warning"></i></a>
 											<a href="#" class="table-action-btn" ng-click="deleteUser(u.user_id)">
 												<i class="md md-delete text-danger"></i></a>
@@ -139,7 +139,7 @@
 						<label class="col-lg-2 control-label " for="userName2">Username *</label>
 						<div class="col-lg-10">
 							<input class=" form-control required" id="user_name"
-								name="user_name" type="text" ng-model="user_name" value={{user_name}}>
+								name="user_name" type="text" value={{user_name}}>
 						</div>
 					</div>
 					<!-- User Password -->
@@ -147,7 +147,7 @@
 						<label class="col-lg-2 control-label " for="password2">Password *</label>
 						<div class="col-lg-10">
 							<input id="password2" name="password" type="password"
-								class="required form-control" ng-model="password" value={{password}}>
+								class="required form-control" value={{password}}>
 						</div>
 					</div>
 
@@ -156,7 +156,7 @@
 						<label class="col-lg-2 control-label " for="email2">Email *</label>
 						<div class="col-lg-10">
 							<input id="email2" name="email" type="text"
-								class="required email form-control" ng-model="email" value={{email}}>
+								class="required email form-control" value={{email}}>
 						</div>
 					</div>
 
@@ -165,7 +165,7 @@
 						<label class="col-lg-2 control-label " for="phone">Contact Number *</label>
 						<div class="col-lg-10">
 							<input id="phone" name="phone" type="text" placeholder="Ex: 012 345 678"
-								class="required form-control" ng-model="contact" value={{contact}}>
+								class="required form-control" value={{contact}}>
 						</div>
 					</div>
 			
@@ -173,7 +173,7 @@
 						<label class="col-lg-2 control-label " for="fname">First Name *</label>
 						<div class="col-lg-10">
 							<input id="fname" name="fname" type="text"
-								class="form-control" ng-model="first_name" value={{first_name}}>
+								class="form-control" value={{first_name}}>
 						</div>		
 					</div>
 					
@@ -181,14 +181,14 @@
 						<label class="col-lg-2 control-label " for="lname">Last Name *</label>
 						<div class="col-lg-10">
 							<input id="lname" name="lname" type="text"
-								class="form-control" ng-model="last_name" value={{last_name}}>
+								class="form-control" value={{last_name}}>
 						</div>	
 					</div>
 					
 					<div class="form-group row">
 						<label class="col-lg-2 control-label " for="lname">Gender *</label>
 						<div class="col-lg-10">
-							<select class="form-control select2" name="gender" ng-model="gender" value={{gender}}>
+							<select class="form-control select2" name="gender" value={{gender}}>
 								<option value="female">Female</option>
 								<option value="male">Male</option>
 							</select>
@@ -200,7 +200,7 @@
 						<div class="col-lg-10">
 							<div class="input-group">
 								<input type="text" class="required form-control"
-									placeholder="mm/dd/yyyy" id="datepicker" ng-model="dob" value={{dob}}> 
+									placeholder="dd-MM-yyyyd" id="datepicker" value={{dob}}> 
 								<span class="input-group-addon bg-custom b-0 text-white"><i
 									class="icon-calender"></i></span>
 							</div>
@@ -212,14 +212,14 @@
 						<label class="col-lg-2 control-label">Address</label>
 						<div class="col-lg-10">
 							<textarea class="required form-control" rows="5"
-								ng-model="address" value={{address}}></textarea>
+								 value={{address}}></textarea>
 						</div>
 					</div>
 					
 					<div class="form-group row">
 						<label class="col-lg-2 control-label">Photo</label>
 						<div class="col-lg-10 dropzone dz-clickable" id="dropzone">
-							<div class="dz-default dz-message" ng-model="photo" value={{photo}}></div>
+							<div class="dz-default dz-message" value={{photo}}></div>
 						</div>
 					</div>
 					
@@ -228,12 +228,12 @@
 						<div class="col-lg-10">
 							<div class="radio radio-info radio-inline">
 								<input type="radio" id="role1" name="role"
-									ng-model="type" value="admin"> 
+									value="admin"> 
 									<label for="role1"> Admin </label>
 							</div>
 							<div class="radio radio-inline">
 								<input type="radio" id="role2" name="role"
-									ng-model="type" value="bidder" > 
+									value="bidder" > 
 									<label for="role2"> Bidder </label>
 							</div>
 						</div>
@@ -244,12 +244,12 @@
 						<div class="col-lg-10">
 							<div class="radio radio-info radio-inline">
 								<input type="radio" id="status1" name="status"
-									ng-model="status" value="1" > <label for="status1">
+									value="1" > <label for="status1">
 									Enable </label>
 							</div>
 							<div class="radio radio-inline">
 								<input type="radio" id="status2" name="status" 
-									ng-model="status" value="0" > <label
+									value="0" > <label
 									for="status2"> Disable </label>
 							</div>
 						</div>
@@ -258,7 +258,7 @@
 					<div class="form-group row">
 						<label class="col-lg-2 control-label">Comment</label>
 						<div class="col-lg-10">
-							<textarea class="form-control" rows="5" ng-model="comment" value={{comment}}></textarea>
+							<textarea class="form-control" rows="5" value={{comment}}></textarea>
 						</div>
 					</div>
 					
@@ -279,5 +279,6 @@
 
 <!-- footer -->
 <jsp:include page="footer.jsp"></jsp:include>
+
 <script type="text/javascript"
 	src="${pageContext.request.contextPath}/resources/scripts/user-angular.js"></script>
--- src/main/webapp/resources/scripts/user-angular.js
+++ src/main/webapp/resources/scripts/user-angular.js
@@ -4,286 +4,342 @@
 
 var app = angular.module('myApp', []);
 
+app.controller('auctionCtrl', function() {
 
-app.controller('auctionCtrl', function(){
-	
 })
 
-//TODO: VIEW USER BACK END
-app.controller('viewUserCtrl', function($scope,$http,$rootScope){
+// TODO: VIEW USER BACK END
+app.controller('viewUserCtrl', function($scope, $http, $rootScope) {
+	
+	
 	// select all record to display
-	$scope.userName="";
+	$scope.userName = "";
 	var checkPagination = true;
 	var currentPage = 1;
-	
-	$scope.getAllUsers = function(){
-//		alert($scope.pages);
-		$http({
-			url: '/rest/user?limit=' + 5 +"&page=" + currentPage + "&userName="+$scope.userName,
-			method: 'GET',
-			
-		}).then(function(response){
+
+	$scope.getAllUsers = function() {
+		// alert($scope.pages);
+		$http(
+				{
+					url : '/rest/user',
+					method : 'GET',
+
+				}).then(function(response) {
 			$scope.user = response.data.DATA;
-			$scope.pages = response.data.PAGINATION.PAGE;
-			$scope.totalpages = response.data.PAGINATION.TOTAL_PAGES;
-			$scope.totalcount = response.data.PAGINATION.TOTAL_COUNT;
-			if(checkPagination){
-				$scope.setPagination(response.data.PAGINATION);
-				checkPagination = false;
-			}
-			
+//			$scope.pages = response.data.PAGINATION.PAGE;
+//			$scope.totalpages = response.data.PAGINATION.TOTAL_PAGES;
+//			$scope.totalcount = response.data.PAGINATION.TOTAL_COUNT;
+//			if (checkPagination) {
+//				$scope.setPagination(response.data.PAGINATION);
+//				checkPagination = false;
+//			}
+
 		});
 	}
-	
-	//TODO: CTEATE PAGINATION BUTTON
-	$scope.setPagination = function(pagination){
-		console.log("PAGINATION==>", pagination);
-		$("#PAGINATION").bootpag({
-	        total: pagination.TOTAL_PAGES,
-	        page: pagination.PAGE,
-	        maxVisible: 10,
-	        leaps: true,
-	        firstLastUse: true,
-	        first: 'First',
-	        last: 'Last',
-	        wrapClass: 'pagination',
-	        activeClass: 'active',
-	        disabledClass: 'disabled',
-	        nextClass: 'next',
-	        prevClass: 'prev',
-	        lastClass: 'last',
-	        firstClass: 'first'
-	    }); 
-		$("#PAGINATION ul").addClass("pagination");
-	}
-	
-	$('#PAGINATION').bootpag().on("page", function(event, page){
-		checkPagination = false;
-		currentPage = page;
-		$scope.getAllUsers();
-	});
-	
+
+	// TODO: CTEATE PAGINATION BUTTON
+//	$scope.setPagination = function(pagination) {
+//		console.log("PAGINATION==>", pagination);
+//		$("#PAGINATION").bootpag({
+//			total : pagination.TOTAL_PAGES,
+//			page : pagination.PAGE,
+//			maxVisible : 10,
+//			leaps : true,
+//			firstLastUse : true,
+//			first : 'First',
+//			last : 'Last',
+//			wrapClass : 'pagination',
+//			activeClass : 'active',
+//			disabledClass : 'disabled',
+//			nextClass : 'next',
+//			prevClass : 'prev',
+//			lastClass : 'last',
+//			firstClass : 'first'
+//		});
+//		$("#PAGINATION ul").addClass("pagination");
+//	}
+//
+//	$('#PAGINATION').bootpag().on("page", function(event, page) {
+//		checkPagination = false;
+//		currentPage = page;
+//		$scope.getAllUsers();
+//	});
+
 	// load all record
-	
+
 	$scope.getAllUsers();
 })
 
-//TODO: ADD DELETE UPDATE BACK END
-app.controller('UserCtrl', function($scope,$http,$rootScope){
+// TODO: ADD DELETE UPDATE BACK END
+app.controller('UserCtrl', function($scope, $http, $rootScope) {
 	// add record function
-	$scope.addUser = function(){
+	$scope.addUser = function() {
 		$scope.created_date = new Date();
 		$scope.created_by = 'admin';
-		$http({
-			url: '/rest/user',
-			method: 'POST',
-			data:{
-				  "address": $scope.address,
-	  			  "comment": ($scope.comment==undefined)?'New':$scope.comment,
-	  			  "contact": $scope.contact,
-	  			  "created_by": $scope.created_by,
-	  			  "created_date": $scope.created_date,
-	  			  "dob": moment($('#datepicker').val()).format("YYYY-MM-DD"),
-	  			  "email": $scope.email,
-	  			  "first_name": $scope.first_name,
-	  			  "gender": $('.select2').val(),
-	  			  "last_name": $scope.last_name,
-	  			  "password": $scope.password,
-	  			  "photo": ($scope.photo==undefined)?'String':$scope.photo,
-	  			  "status": ($scope.status==undefined)?'1':$("input:radio[name=status]:checked").val(),
-	  			  "type": ($scope.type == undefined)?'bidder': $("input:radio[name=role]:checked").val(),
-	  			  "user_name": $scope.user_name
-			}
-		}).then(function(respone){
-			swal({ 
-				title: "Success!",
-				text: "User has been inserted.",
-			    type: "success",
-			    timer : 1000,
-			    showConfirmButton : false
-			  },
-			  function(){
-			    window.location.href = '/admin/viewuser';
+		$http(
+				{
+					url : '/rest/user',
+					method : 'POST',
+					data : {
+						"address" : $scope.address,
+						"comment" : ($scope.comment == undefined) ? 'New'
+								: $scope.comment,
+						"contact" : $scope.contact,
+						"created_by" : $scope.created_by,
+						"created_date" : $scope.created_date,
+						"dob" : moment($('#datepicker').val()).format(
+								"YYYY-MM-DD"),
+						"email" : $scope.email,
+						"first_name" : $scope.first_name,
+						"gender" : $('.select2').val(),
+						"last_name" : $scope.last_name,
+						"password" : $scope.password,
+						"photo" : ($scope.photo == undefined) ? 'String'
+								: $scope.photo,
+						"status" : ($scope.status == undefined) ? '1' : $(
+								"input:radio[name=status]:checked").val(),
+						"type" : ($scope.type == undefined) ? 'bidder' : $(
+								"input:radio[name=role]:checked").val(),
+						"user_name" : $scope.user_name
+					}
+				}).then(function(respone) {
+			swal({
+				title : "Success!",
+				text : "User has been inserted.",
+				type : "success",
+				timer : 1000,
+				showConfirmButton : false
+			}, function() {
+				window.location.href = '/admin/viewuser';
 			});
-			
+
 		});
 	}
-	
+
 	// Get User By ID
-	$scope.getUserByID = function(userObject){
-		$scope.user_name = userObject.user_name;
-		$scope.password = userObject.password;
-		$scope.email = userObject.email;
-		$scope.contact = userObject.contact;
-		$scope.first_name = userObject.first_name;
-		$scope.last_name = userObject.last_name;
-		
-		if((userObject.gender=='female') || (userObject.gender=='F')){
+	// $scope.getUserByID = function(userObject){
+	// alert(userObject.user_name);
+	// $scope.user_name = userObject.user_name;
+	// $scope.password = userObject.password;
+	// $scope.email = userObject.email;
+	// $scope.contact = userObject.contact;
+	// $scope.first_name = userObject.first_name;
+	// $scope.last_name = userObject.last_name;
+	//		
+	// if((userObject.gender=='female') || (userObject.gender=='F')){
+	//
+	// $('select[name="gender"]').find('option[value="female"]').attr("selected",true);
+	// }else{
+	//
+	// $('select[name="gender"]').find('option[value="male"]').attr("selected",true);
+	// }
+	// $scope.dob = userObject.dob;
+	// $scope.address = userObject.address;
+	// $scope.photo = userObject.photo;
+	// if(userObject.type=='admin'){
+	// $( "#role1" ).prop( "checked", true);
+	// }
+	// else{
+	// $( "#role2" ).prop( "checked", true);
+	// }
+	// if(userObject.status=='1'){
+	// $( "#status1" ).prop( "checked", true);
+	// }
+	// else{
+	// $( "#status2" ).prop( "checked", true);
+	// }
+	// $scope.comment = userObject.comment;
+	//		
+	// $scope.user_id = userObject.user_id;
+	//			
+	// }
 
-			$('select[name="gender"]').find('option[value="female"]').attr("selected",true);
-		}else{
+	$scope.getUserByID = function(id) {
+		alert(id);
+		$http({
+			url : '/rest/user/' + id,
+			method : 'GET'
+		}).then(
+				function(response) {
+					$scope.user_name = response.data.DATA.user_name;
+					$scope.password = response.data.DATA.password;
+					$scope.email = response.data.DATA.email;
+					$scope.contact = response.data.DATA.contact;
+					$scope.first_name = response.data.DATA.first_name;
+					$scope.last_name = response.data.DATA.last_name;
+					if ((response.data.DATA.gender == 'female')
+							|| (userObject.gender == 'F')) {
 
-			$('select[name="gender"]').find('option[value="male"]').attr("selected",true);
-		}
-		$scope.dob = userObject.dob;
-		$scope.address = userObject.address;
-		$scope.photo = userObject.photo;
-		if(userObject.type=='admin'){
-			$( "#role1" ).prop( "checked", true);
-		}
-		else{
-			$( "#role2" ).prop( "checked", true);
-		}
-		if(userObject.status=='1'){
-			$( "#status1" ).prop( "checked", true);
-		}
-		else{
-			$( "#status2" ).prop( "checked", true);
-		}
-		$scope.comment = userObject.comment;
-		
-		$scope.user_id = userObject.user_id;
-			
+						$('select[name="gender"]').find(
+								'option[value="female"]')
+								.attr("selected", true);
+					} else {
+
+						$('select[name="gender"]').find('option[value="male"]')
+								.attr("selected", true);
+					}
+					$scope.dob = response.data.DATA.dob;
+					$scope.address = response.data.DATA.address;
+					$scope.photo = response.data.DATA.photo;
+					if (response.data.DATA.type == 'admin') {
+						$("#role1").prop("checked", true);
+					} else {
+						$("#role2").prop("checked", true);
+					}
+					if(response.data.DATA.status=='1'){
+						 $("#status1").prop("checked", true);
+					} else {
+						$("#status2").prop("checked", true);
+					}
+					$scope.comment = response.data.DATA.comment;
+				})
 	}
-	
-	//Update User
-	$scope.updateUser = function(id){
+	// Update User
+	$scope.updateUser = function(id) {
 		$scope.created_date = new Date();
 		$scope.created_by = 'admin';
-		$http({
-			url: '/rest/user',
-			method: 'PUT',
-			data:{
-				  "user_id" : id,
-				  "address": $scope.address,
-	  			  "comment": $scope.comment,
-				  "contact": $scope.contact,
-	  			  "created_by": $scope.created_by,
-	  			  "created_date": $scope.created_date,
-	  			  "dob": moment($('#datepicker').val()).format("YYYY-MM-DD"),
-	  			  "email": $scope.email,
-	  			  "first_name": $scope.first_name,
-	  			  "gender": $('.select2').val(),
-	  			  "last_name": $scope.last_name,
-	  			  "password": $scope.password,
-	  			  "photo": $scope.photo,
-	  			  "status": $("input:radio[name=status]:checked").val(),
-	  			  "type": $("input:radio[name=role]:checked").val(),	  			  
-	  			  "user_name": $scope.user_name,
-	  			  "role_id": ($("input:radio[name=status]:checked").val()=='admin')?1:2
-			}
-		}).then(function(response){
+		$http(
+				{
+					url : '/rest/user',
+					method : 'PUT',
+					data : {
+						"user_id" : id,
+						"address" : $scope.address,
+						"comment" : $scope.comment,
+						"contact" : $scope.contact,
+						"created_by" : $scope.created_by,
+						"created_date" : $scope.created_date,
+						"dob" : moment($('#datepicker').val()).format(
+								"YYYY-MM-DD"),
+						"email" : $scope.email,
+						"first_name" : $scope.first_name,
+						"gender" : $('.select2').val(),
+						"last_name" : $scope.last_name,
+						"password" : $scope.password,
+						"photo" : $scope.photo,
+						"status" : $("input:radio[name=status]:checked").val(),
+						"type" : $("input:radio[name=role]:checked").val(),
+						"user_name" : $scope.user_name,
+						"role_id" : ($("input:radio[name=status]:checked")
+								.val() == 'admin') ? 1 : 2
+					}
+				}).then(function(response) {
 			swal({
-				title : "Success", 
-				text : "User has been updated!", 
+				title : "Success",
+				text : "User has been updated!",
 				type : "success",
 				timer : 1000,
 				showConfirmButton : false
 			});
 			$scope.getAllUsers();
-		},function(error){});
+		}, function(error) {
+		});
 	}
-	
-	//Get user_name and password to login
-	$scope.userLogin = function(user_name,password){
-		$http({  
-			url:'/rest/user',
-			method: 'POST',
-			data:{
-				"user_name": $scope.user_name,
-				"password": $scope.password
+
+	// Get user_name and password to login
+	$scope.userLogin = function(user_name, password) {
+		$http({
+			url : '/rest/user',
+			method : 'POST',
+			data : {
+				"user_name" : $scope.user_name,
+				"password" : $scope.password
 			}
 		})
 	}
-	
+
 	// delete supplier
-	$scope.deleteUser = function(id){	
+	$scope.deleteUser = function(id, e) {
+		
+		console.log(id);	
 		
 		swal({
-		title: "Are you sure?",
-		text: "Your will not be able to recover this user!",
-		type: "warning",
-		showCancelButton: true,
-		confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, delete it!",
-		cancelButtonText: "No, cancel plx!",
-		closeOnConfirm: false,
-		closeOnCancel: false }, 
-		function(isConfirm){ 
+			title : "Are you sure?",
+			text : "Your will not be able to recover this user!",
+			type : "warning",
+			showCancelButton : true,
+			confirmButtonColor : "#DD6B55",
+			confirmButtonText : "Yes, delete it!",
+			cancelButtonText : "No, cancel plx!",
+			closeOnConfirm : false,
+			closeOnCancel : false
+		}, function(isConfirm) {
 			if (isConfirm) {
 				$http({
-					url: '/rest/user/'+id,
-					
-					method: 'DELETE'
-				}).
-				success(function(response){
+					url : '/rest/user/' + id,
+
+					method : 'DELETE'
+				}).success(function(response) {
 					$scope.getAllUsers();
-					});
+				});
 				swal({
-					title : "Deleted!", 
-					text : "The user has been deleted.", 
+					title : "Deleted!",
+					text : "The user has been deleted.",
 					type : "success",
 					timer : 1000,
 					showConfirmButton : false
 				});
 
-				//$scope.findAllSuppliers();
+				// $scope.findAllSuppliers();
 			} else {
 				swal({
-					title : "Cancelled!", 
-					text : "Your user is not deleted :)", 
+					title : "Cancelled!",
+					text : "Your user is not deleted :)",
 					type : "error",
 					timer : 1000,
 					showConfirmButton : false
 				});
 			}
-		});					
+		});
 	}
-	
-	
-//	$scope.addUser();
+
+	// $scope.addUser();
 })
 
-
-
-//========================================================================================
-//TODO: ADD USER FRONT END
-app.controller('addUserCtrl', function($scope,$http,$rootScope){
+// ========================================================================================
+// TODO: ADD USER FRONT END
+app.controller('addUserCtrl', function($scope, $http, $rootScope) {
 	// add record function
-	$scope.addUser = function(){
+	$scope.addUser = function() {
 		$scope.created_date = new Date();
 		$scope.created_by = 'admin';
-		$http({
-			url: '/rest/user',
-			method: 'POST',
-			data:{
-				  "address": $scope.address,
-	  			  "comment": ($scope.comment==undefined)?'New':$scope.comment,
-	  			  "contact": $scope.contact,
-	  			  "created_by": $scope.created_by,
-	  			  "created_date": $scope.created_date,
-	  			  "dob": moment($('#datepicker').val()).format("YYYY-MM-DD"),
-	  			  "email": $scope.email,
-	  			  "first_name": $scope.first_name,
-	  			  "gender": $('.select2').val(),
-	  			  "last_name": $scope.last_name,
-	  			  "password": $scope.password,
-	  			  "photo": ($scope.photo==undefined)?'String':$scope.photo,
-	  			  "status": ($scope.status==undefined)?'1':$("input:radio[name=status]:checked").val(),
-	  			  "type": ($scope.type == undefined)?'bidder': $("input:radio[name=role]:checked").val(),
-	  			  "user_name": $scope.user_name
-			}
-		}).then(function(respone){
-			swal({ 
-				title: "Success!",
-				text: "User has been inserted.",
-			    type: "success",
-			    timer : 1000,
-			    showConfirmButton : false
-			  },
-			  function(){
-			    window.location.href = '/';
+		$http(
+				{
+					url : '/rest/user',
+					method : 'POST',
+					data : {
+						"address" : $scope.address,
+						"comment" : ($scope.comment == undefined) ? 'New'
+								: $scope.comment,
+						"contact" : $scope.contact,
+						"created_by" : $scope.created_by,
+						"created_date" : $scope.created_date,
+						"dob" : moment($('#datepicker').val()).format(
+								"YYYY-MM-DD"),
+						"email" : $scope.email,
+						"first_name" : $scope.first_name,
+						"gender" : $('.select2').val(),
+						"last_name" : $scope.last_name,
+						"password" : $scope.password,
+						"photo" : ($scope.photo == undefined) ? 'String'
+								: $scope.photo,
+						"status" : ($scope.status == undefined) ? '1' : $(
+								"input:radio[name=status]:checked").val(),
+						"type" : ($scope.type == undefined) ? 'bidder' : $(
+								"input:radio[name=role]:checked").val(),
+						"user_name" : $scope.user_name
+					}
+				}).then(function(respone) {
+			swal({
+				title : "Success!",
+				text : "User has been inserted.",
+				type : "success",
+				timer : 1000,
+				showConfirmButton : false
+			}, function() {
+				window.location.href = '/';
 			});
-			
+
 		});
 	}
 })
